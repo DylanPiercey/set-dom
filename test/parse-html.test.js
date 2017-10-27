f7725307b4460019a26402f60629726fc38f63f2
Max Mathieu <max@collage.com>
2017-10-27 10:18:22 -0400

    Better test isolation/cleaner override restore

--- test/parse-html.test.js
+++ test/parse-html.test.js
@@ -3,7 +3,7 @@
 var assert = require('assert')
 
 describe('Parse-html helper', function () {
-  var runTest = function () {
+  var runTests = function () {
     delete require.cache[require.resolve('../src/parse-html')]
     var parseHTML = require('../src/parse-html')
     var divHtml = '<div class="someClass">Test</div>'
@@ -25,74 +25,107 @@ describe('Parse-html helper', function () {
 
   it('should use DOMParser', function () {
     // DOMParser is natively supported in the test environment
-    runTest()
+    runTests()
   })
 
-  it('should handle browsers not supporting DOMParser', function () {
-    var oldFn = window.DOMParser
-    delete window.DOMParser
-    runTest()
-    window.DOMParser = oldFn
+  describe('Without DOMParser', function () {
+    var oldFn
+    before(function () {
+      oldFn = window.DOMParser
+      delete window.DOMParser
+    })
+    after(function () {
+      window.DOMParser = oldFn
+    })
+
+    it('should still work', function () {
+      runTests()
+    })
   })
 
-  it('should handle browsers not supporting DOMParser with text/html', function () {
-    var oldFn = window.DOMParser.prototype.parseFromString
-    window.DOMParser.prototype.parseFromString = function (markup, type) {
-      if (type === 'text/html') { throw new Error('text/html not supported') }
-      // in the test environment, the xhtml parsing is not supported, use html instead
-      return oldFn.call(this, markup, 'text/html')
-    }
-    runTest()
-    window.DOMParser.prototype.parseFromString = oldFn
-  })
-
-  it('should handle browsers not supporting documentElement.innerHTML either', function () {
-    var oldFn = window.DOMParser.prototype.parseFromString
-    window.DOMParser.prototype.parseFromString = function (markup, type) {
-      if (type === 'text/html') { throw Error('text/html not supported') }
-      // in the test environment, the xhtml parsing is not supported, use html instead
-      return oldFn.call(this, markup, 'text/html')
-    }
-    var oldDesc = Object.getOwnPropertyDescriptor(Element.prototype, 'innerHTML')
-    Object.defineProperty(Element.prototype, 'innerHTML', {
-      get: function () {
-        return oldDesc.get.call(this)
-      },
-      set: function () {
-        if (this.nodeName === 'HTML') { throw new Error('not supported') }
-        return oldDesc.set.apply(this, arguments)
+  describe('With DOMParser, but without text/html support', function () {
+    var oldFn
+    before(function () {
+      oldFn = window.DOMParser.prototype.parseFromString
+      window.DOMParser.prototype.parseFromString = function (markup, type) {
+        if (type === 'text/html') { throw new Error('text/html not supported') }
+        // in the test environment, the xhtml parsing is not supported, use html instead
+        return oldFn.call(this, markup, 'text/html')
       }
     })
-    runTest()
-    window.DOMParser.prototype.parseFromString = oldFn
-    Object.defineProperty(Element.prototype, 'innerHTML', oldDesc)
+    after(function () {
+      window.DOMParser.prototype.parseFromString = oldFn
+    })
+
+    it('should still work', function () {
+      runTests()
+    })
+
+    describe('AND without support for documentElement.innerHTML', function () {
+      var oldDesc
+      before(function () {
+        oldDesc = Object.getOwnPropertyDescriptor(Element.prototype, 'innerHTML')
+        Object.defineProperty(Element.prototype, 'innerHTML', {
+          get: function () {
+            return oldDesc.get.call(this)
+          },
+          set: function () {
+            if (this.nodeName === 'HTML') { throw new Error('not supported') }
+            return oldDesc.set.apply(this, arguments)
+          }
+        })
+      })
+      after(function () {
+        Object.defineProperty(Element.prototype, 'innerHTML', oldDesc)
+      })
+
+      it('should still work', function () {
+        runTests()
+      })
+    })
   })
 
-  it('should handle iOS 9 Safari / Safari 9 lowering classes', function () {
-    var oldFn = Element.prototype.appendChild
-    Element.prototype.appendChild = function (child) {
-      // this is essentially what iOS 9 Safari and Safari 9 do:
-      // lowercase classList but keep html unchanged
-      for (var i = 0; i < child.classList.length; i++) { child.classList[i] = child.classList[i].toLowerCase() }
-      oldFn.call(this, child)
-    }
-    runTest()
-    Element.prototype.appendChild = oldFn
-  })
-
-  it('should handle iOS UIWebView sometimes failing', function () {
-    var oldFn = window.DOMParser.prototype.parseFromString
-    window.DOMParser.prototype.parseFromString = function (data, type) {
-      var parsed = oldFn.apply(this, arguments)
-      if (parsed.body.firstChild && parsed.body.firstChild.nodeName === 'WBR') {
-        // make it pass the testCode in parse-html
-        return parsed
-      } else {
-        // this is essentially what iOS UIWebView sometimes returns
-        return { body: null }
+  describe('With weird className lowercasing (iOS 9 Safari / Safari 9)', function () {
+    var oldFn
+    before(function () {
+      oldFn = Element.prototype.appendChild
+      Element.prototype.appendChild = function (child) {
+        // this is essentially what iOS 9 Safari and Safari 9 do:
+        // lowercase classList but keep html unchanged
+        for (var i = 0; i < child.classList.length; i++) { child.classList[i] = child.classList[i].toLowerCase() }
+        oldFn.call(this, child)
       }
-    }
-    runTest()
-    window.DOMParser.prototype.parseFromString = oldFn
+    })
+    after(function () {
+      Element.prototype.appendChild = oldFn
+    })
+
+    it('should still work', function () {
+      runTests()
+    })
+  })
+
+  describe('With random DOMParser failures (iOS UIWebView)', function () {
+    var oldFn
+    before(function () {
+      oldFn = window.DOMParser.prototype.parseFromString
+      window.DOMParser.prototype.parseFromString = function (data, type) {
+        var parsed = oldFn.apply(this, arguments)
+        if (parsed.body.firstChild && parsed.body.firstChild.nodeName === 'WBR') {
+          // make it pass the testCode in parse-html
+          return parsed
+        } else {
+          // this is essentially what iOS UIWebView sometimes returns
+          return { body: null }
+        }
+      }
+    })
+    after(function () {
+      window.DOMParser.prototype.parseFromString = oldFn
+    })
+
+    it('should still work', function () {
+      runTests()
+    })
   })
 })
